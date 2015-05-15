2c348753c3fa42fa8f72abe544f11dbc88776e8c
Saminou <spam+github@anthologeek.net>
2015-05-15 15:17:30 -0400

    Add go-routine unsafe version in case unnecessary or locking job happens above

---
+++ unsafe/unsafecache.go
@@ -0,0 +1,271 @@
+// This package provide a simple memory k/v cache and LRU cache.
+// It based on the k/v cache implementation in go-cache:
+// https://github.com/pmylund/go-cache
+// and LRU cache implementation in groupcache:
+// https://github.com/golang/groupcache/tree/master/lru
+package unsafecache
+
+import (
+	"container/list"
+	"errors"
+	"fmt"
+	"time"
+)
+
+// Cache is a goroutine-safe K/V cache.
+type Cache struct {
+	items             map[interface{}]*Item
+	defaultExpiration time.Duration
+}
+
+type Item struct {
+	Object     interface{}
+	Expiration *time.Time
+}
+
+// Returns true if the item has expired.
+func (item *Item) Expired() bool {
+	if item.Expiration == nil {
+		return false
+	}
+	return item.Expiration.Before(time.Now())
+}
+
+// New create a new cache with a given default expiration duration and cleanup
+// interval. If the expiration duration is less than 1, the items in the cache
+// never expire (by default), and must be deleted manually. If the cleanup
+// interval is less than one, expired items are not deleted from the cache
+// before calling DeleteExpired.
+func New(defaultExpiration, cleanInterval time.Duration) *Cache {
+	c := &Cache{
+		items:             map[interface{}]*Item{},
+		defaultExpiration: defaultExpiration,
+	}
+	if cleanInterval > 0 {
+		go func() {
+			for {
+				time.Sleep(cleanInterval)
+				c.DeleteExpired()
+			}
+		}()
+	}
+	return c
+}
+
+// Get return an item or nil, and a bool indicating whether
+// the key was found.
+func (c *Cache) Get(key interface{}) (interface{}, bool) {
+	item, ok := c.items[key]
+	if !ok || item.Expired() {
+		return nil, false
+	}
+	return item.Object, true
+}
+
+// Set add a new key or replace an exist key. If the dur is 0, we will
+// use the defaultExpiration.
+func (c *Cache) Set(key interface{}, val interface{}, dur time.Duration) {
+	var t *time.Time
+	if dur == 0 {
+		dur = c.defaultExpiration
+	}
+	if dur > 0 {
+		tmp := time.Now().Add(dur)
+		t = &tmp
+	}
+	c.items[key] = &Item{
+		Object:     val,
+		Expiration: t,
+	}
+}
+
+// Delete a key-value pair if the key is existed.
+func (c *Cache) Delete(key interface{}) {
+	delete(c.items, key)
+}
+
+// Delete all cache.
+func (c *Cache) Flush() {
+	c.items = map[interface{}]*Item{}
+}
+
+// Add a number to a key-value pair.
+func (c *Cache) Increment(key interface{}, x int64) error {
+	val, ok := c.items[key]
+	if !ok || val.Expired() {
+		return fmt.Errorf("Item %s not found", key)
+	}
+	switch val.Object.(type) {
+	case int:
+		val.Object = val.Object.(int) + int(x)
+	case int8:
+		val.Object = val.Object.(int8) + int8(x)
+	case int16:
+		val.Object = val.Object.(int16) + int16(x)
+	case int32:
+		val.Object = val.Object.(int32) + int32(x)
+	case int64:
+		val.Object = val.Object.(int64) + x
+	case uint:
+		val.Object = val.Object.(uint) + uint(x)
+	case uint8:
+		val.Object = val.Object.(uint8) + uint8(x)
+	case uint16:
+		val.Object = val.Object.(uint16) + uint16(x)
+	case uint32:
+		val.Object = val.Object.(uint32) + uint32(x)
+	case uint64:
+		val.Object = val.Object.(uint64) + uint64(x)
+	case uintptr:
+		val.Object = val.Object.(uintptr) + uintptr(x)
+	default:
+		return fmt.Errorf("The value type error")
+	}
+	return nil
+}
+
+// Sub a number to a key-value pair.
+func (c *Cache) Decrement(key interface{}, x int64) error {
+	val, ok := c.items[key]
+	if !ok || val.Expired() {
+		return fmt.Errorf("Item %s not found", key)
+	}
+	switch val.Object.(type) {
+	case int:
+		val.Object = val.Object.(int) - int(x)
+	case int8:
+		val.Object = val.Object.(int8) - int8(x)
+	case int16:
+		val.Object = val.Object.(int16) - int16(x)
+	case int32:
+		val.Object = val.Object.(int32) - int32(x)
+	case int64:
+		val.Object = val.Object.(int64) - x
+	case uint:
+		val.Object = val.Object.(uint) - uint(x)
+	case uint8:
+		val.Object = val.Object.(uint8) - uint8(x)
+	case uint16:
+		val.Object = val.Object.(uint16) - uint16(x)
+	case uint32:
+		val.Object = val.Object.(uint32) - uint32(x)
+	case uint64:
+		val.Object = val.Object.(uint64) - uint64(x)
+	case uintptr:
+		val.Object = val.Object.(uintptr) - uintptr(x)
+	default:
+		return fmt.Errorf("The value type error")
+	}
+	return nil
+}
+
+// Return the number of item in cache.
+func (c *Cache) ItemCount() int {
+	counts := len(c.items)
+	return counts
+}
+
+// Delete all expired items.
+func (c *Cache) DeleteExpired() {
+	for k, v := range c.items {
+		if v.Expired() {
+			delete(c.items, k)
+		}
+	}
+}
+
+// The LRUCache is a goroutine-safe cache.
+type LRUCache struct {
+	maxEntries int
+	items      map[interface{}]*list.Element
+	cacheList  *list.List
+}
+
+type entry struct {
+	key   interface{}
+	value interface{}
+}
+
+// NewLRU create a LRUCache with max size. The size is 0 means no limit.
+func NewLRU(size int) (*LRUCache, error) {
+	if size < 0 {
+		return nil, errors.New("The size of LRU Cache must no less than 0")
+	}
+	lru := &LRUCache{
+		maxEntries: size,
+		items:      make(map[interface{}]*list.Element, size),
+		cacheList:  list.New(),
+	}
+	return lru, nil
+}
+
+// Add a new key-value pair to the LRUCache.
+func (c *LRUCache) Add(key interface{}, value interface{}) {
+	if ent, hit := c.items[key]; hit {
+		c.cacheList.MoveToFront(ent)
+		ent.Value.(*entry).value = value
+		return
+	}
+	ent := &entry{
+		key:   key,
+		value: value,
+	}
+	entry := c.cacheList.PushFront(ent)
+	c.items[key] = entry
+
+	if c.maxEntries > 0 && c.cacheList.Len() > c.maxEntries {
+		c.removeOldestElement()
+	}
+}
+
+// Get a value from the LRUCache. And a bool indicating
+// whether found or not.
+func (c *LRUCache) Get(key interface{}) (interface{}, bool) {
+	if ent, hit := c.items[key]; hit {
+		c.cacheList.MoveToFront(ent)
+		return ent.Value.(*entry).value, true
+	}
+	return nil, false
+}
+
+// Remove a key-value pair in LRUCache. If the key is not existed,
+// nothing will happen.
+func (c *LRUCache) Remove(key interface{}) {
+	if ent, hit := c.items[key]; hit {
+		c.removeElement(ent)
+	}
+}
+
+// Return the number of key-value pair in LRUCache.
+func (c *LRUCache) Len() int {
+	length := c.cacheList.Len()
+	return length
+}
+
+// Delete all entry in the LRUCache. But the max size will hold.
+func (c *LRUCache) Clear() {
+	c.cacheList = list.New()
+	c.items = make(map[interface{}]*list.Element, c.maxEntries)
+}
+
+// Resize the max limit.
+func (c *LRUCache) SetMaxEntries(max int) error {
+	if max < 0 {
+		return errors.New("The max limit of entryies must no less than 0")
+	}
+	c.maxEntries = max
+	return nil
+}
+
+func (c *LRUCache) removeElement(e *list.Element) {
+	c.cacheList.Remove(e)
+	ent := e.Value.(*entry)
+	delete(c.items, ent.key)
+}
+
+func (c *LRUCache) removeOldestElement() {
+	ent := c.cacheList.Back()
+	if ent != nil {
+		c.removeElement(ent)
+	}
+}
---
+++ unsafe/unsafecache_test.go
@@ -0,0 +1,223 @@
+package unsafecache
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestCache(t *testing.T) {
+	c := New(2*time.Second, 1*time.Second)
+	_, found := c.Get("xx")
+	if found {
+		t.Error("You should not get")
+	}
+
+	c.Set("key", "lalala", -1)
+	val, found := c.Get("key")
+	if !found {
+		t.Error("You must get this value")
+	}
+	if val != "lalala" {
+		t.Error("You get a wrong value")
+	}
+	c.Delete("key")
+	_, found = c.Get("key")
+	if found {
+		t.Error("The key is delete, you should not get")
+	}
+	c.Set("key", "bababa", -1)
+	cnt := c.ItemCount()
+	if cnt != 1 {
+		t.Error("The number of cache must be 1")
+	}
+	c.Flush()
+	_, found = c.Get("key")
+	if found {
+		t.Error("All keys are flush, you should not get")
+	}
+	cnt = c.ItemCount()
+	if cnt != 0 {
+		t.Error("The number of cache must be 0")
+	}
+
+	c.Set("key", "val", 2*time.Second)
+	_, found = c.Get("key")
+	if !found {
+		t.Error("must have this")
+	}
+	time.Sleep(3 * time.Second)
+	cnt = c.ItemCount()
+	if cnt != 0 {
+		t.Error("The number of cache must be 0")
+	}
+	_, found = c.Get("key")
+	if found {
+		t.Error("The key is time out, you should not get")
+	}
+}
+
+func TestIncDec(t *testing.T) {
+	c := New(1*time.Second, -1)
+	c.Set("key", 100, -1)
+	c.Increment("key", 1)
+	val, _ := c.Get("key")
+	if val != 101 {
+		t.Error("Increment error")
+	}
+	c.Decrement("key", 1)
+	val, _ = c.Get("key")
+	if val != 100 {
+		t.Error("Decrement error")
+	}
+}
+
+func BenchmarkCacheGet(b *testing.B) {
+	b.StopTimer()
+	tc := New(0, 0)
+	tc.Set("key", "values", -1)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		tc.Get("key")
+	}
+}
+
+func BenchmarkRWMutexGet(b *testing.B) {
+	b.StopTimer()
+	m := map[string]string{
+		"key": "values",
+	}
+	mu := sync.RWMutex{}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		mu.RLock()
+		_, _ = m["key"]
+		mu.RUnlock()
+	}
+}
+
+func BenchmarkCacheSet(b *testing.B) {
+	b.StopTimer()
+	tc := New(0, 0)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		tc.Set("key", "values", -1)
+	}
+}
+
+func BenchmarkRWMutexSet(b *testing.B) {
+	b.StopTimer()
+	m := map[string]string{}
+	mu := sync.RWMutex{}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		mu.Lock()
+		m["key"] = "values"
+		mu.Unlock()
+	}
+}
+
+func BenchmarkCacheIncrement(b *testing.B) {
+	b.StopTimer()
+	tc := New(0, 0)
+	tc.Set("key", 0, -1)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		tc.Increment("key", 1)
+	}
+}
+
+func BenchmarkCacheDecrement(b *testing.B) {
+	b.StopTimer()
+	tc := New(0, 0)
+	tc.Set("key", 0, -1)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		tc.Decrement("key", 1)
+	}
+}
+
+func TestLRUCache(t *testing.T) {
+	_, err := NewLRU(-1)
+	if err == nil {
+		t.Error("Impossiable!")
+	}
+	lru, err := NewLRU(1)
+	if err != nil {
+		t.Error(err)
+	}
+	lru.Add("1", 111)
+	if lru.Len() != 1 {
+		t.Error("Now, there is one value in cache")
+	}
+	val, hit := lru.Get("1")
+	if !hit {
+		t.Error("I should get the key")
+	}
+	if val != 111 {
+		t.Error("Get the wrong value")
+	}
+	lru.Add("2", 222)
+	if lru.Len() != 1 {
+		t.Error("Now, there is only one value in cache")
+	}
+	_, hit = lru.Get("1")
+	if hit {
+		t.Error("The old value must be removed")
+	}
+	lru.Remove("2")
+	_, hit = lru.Get("2")
+	if hit {
+		t.Error("The value must be removed")
+	}
+	if lru.Len() != 0 {
+		t.Error("Now, there is no value in cache")
+	}
+	lru.Clear()
+	if lru.Len() != 0 {
+		t.Error("Now, the lru cache is cleared")
+	}
+	lru.SetMaxEntries(2)
+	lru.Add("1", 2222)
+	lru.Add("2", 34444)
+	if lru.Len() != 2 {
+		t.Error("Now, the len of lru cache must be 2")
+	}
+}
+
+func ExampleCache() {
+	c := New(0, 0)
+	c.Set("1", 1111, 0)
+	val, found := c.Get("1")
+	if found {
+		fmt.Println(val)
+	}
+	c.Increment("1", 1)
+	val, found = c.Get("1")
+	if found {
+		fmt.Println(val)
+	}
+	// Output:
+	// 1111
+	// 1112
+}
+
+func ExampleLRUCache() {
+	lru, err := NewLRU(3)
+	if err != nil {
+		fmt.Println(err)
+	}
+	lru.Add("1", 1111)
+	lru.Add("2", 2222)
+	lru.Add("3", 3333)
+	lru.Add("4", 4444)
+	_, hit := lru.Get("1")
+	if hit {
+		fmt.Println("Hit key 1")
+	} else {
+		fmt.Println("Not hit key 1")
+	}
+	// Output:
+	// Not hit key 1
+}
